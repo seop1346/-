0bd5cdd22ca23952a6fe338ab8c7a5ed263da8a5
juyoungkimm <31956149+seop1346@users.noreply.github.com>
2017-10-11 14:55:39 +0900

    Update 하트 201601411김주영.py

--- 하트 201601411김주영.py
+++ 하트 201601411김주영.py
@@ -1,5 +1,3 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
 >>> import turtle as t #turtle을 t라고 할 것이다.
 >>> n=50 #n을 50으로 지정
 >>> t.bgcolor("black") #배경색을 검은색으로 지정
@@ -9,6 +7,3 @@ Type "copyright", "credits" or "license()" for more information.
 >>> for x in range(n): #n번 반복
 	t.circle(50) #반지름이 50인 원을 그린다.
 	t.left(200/n) #거북이가 200/n만큼 왼쪽으로 회전
-
-	
->>> 
